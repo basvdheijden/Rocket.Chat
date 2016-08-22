bdb02ed189022a102ed3cc5753dfddca843b800c
Bas van der Heijden <info@basvdheijden.com>
2016-08-22 12:29:03 +0200

    Return only FAQItems for current user

--- packages/rocketchat-livechat/server/publications/faqItems.js
+++ packages/rocketchat-livechat/server/publications/faqItems.js
@@ -1,6 +1,10 @@
 Meteor.publish('livechat:FAQItems', function(roomId) {
+  if (!this.userId) {
+    return this.error(new Meteor.Error('error-not-authorized', 'Not authorized', { publish: 'livechat:FAQItems' }));
+  }
+
   return RocketChat.models.FAQItems.find({
-    rid: roomId
+    "u._id": this.userId
   }, {
     fields: {
       text: 1,
